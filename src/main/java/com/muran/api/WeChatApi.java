b76fec2e6c38f5cdeab7de53ef71329e70f60178
jerry@mrysh.com <jerry@mrysh.com>
2016-07-19 17:00:07 +0800

    同上

--- src/main/java/com/muran/api/WeChatApi.java
+++ src/main/java/com/muran/api/WeChatApi.java
@@ -434,8 +434,8 @@ public class WeChatApi extends AbstractApi {
 			throw new ServerException(Code.BadRequestParams, "临时素材获取失败！");
 		}
 		// 将文件下载到本地
-		String basePath = GlobalConfig.KEY_WEB_BASE;
-		String path = basePath + "upload/wx/" + wechatUser.getAutoId();
+		String basePath =request.getSession().getServletContext().getRealPath("/");
+		String path = basePath + "upload/wx/" + wechatUser.getAutoId()+ "/" +result.getFilename();
 		
 		FileUtil.saveFile(result.getBytes(), path, result.getFilename());
 		String url = GlobalConfig.KEY_WEB_BASE + "upload/wx/"
