1c29d88887f823124e779387e0127b08ffc40a75
jerry@mrysh.com <jerry@mrysh.com>
2016-07-25 12:43:11 +0800

    我的报名

--- src/main/java/com/muran/dao/imp/ActivityDao.java
+++ src/main/java/com/muran/dao/imp/ActivityDao.java
@@ -153,10 +153,9 @@ public class ActivityDao extends AbstractHibernateDao<Activity> implements
 	public  List<ActivityInfo> getActivityWxListByOpenId(Integer num, String upOrDown,
 			Long time,String openid) {
 		String hql = "SELECT a.autoId,a.title,a.overurl as overUrl,a.hoster,a.startTime,a.endTime,a.location,a.publishTime,a.content,a.signupTop,a.status,a.signupEndTime,case when b.signNum is null then 0 else b.signNum end signNum,case when c.commentNum is null then 0 else c.commentNum end commentNum FROM jwhwxt.tb_activity a "
-				+ " left join (select count(activity)as signNum,activity from tb_activity_signup) b on b.activity=a.autoID "
-				+ " left join (select count(itemId)as commentNum,itemId from tb_article_comment where columnKey='column_activities')c on c.itemId=a.autoId"
-				+ " left join tb_activity_signup s on s.activity=a.autoId	"
-				+ " where 1=1 ";
+				+ " left join (select count(activity)as signNum,activity from tb_activity_signup group by activity) b on b.activity=a.autoID "
+				+ " left join (select count(itemId)as commentNum,itemId from tb_article_comment where columnKey='column_activities' group by itemId)c on c.itemId=a.autoId"
+				+ " where 1=1 and a.status=1 ";
 		if (openid != null && !openid.equals("")) {
 			hql += " and s.openid='"+openid+"'";
 		}
